7a48948ddd02dd22e10cd9fbf236a39f207eb409
Raman Buyak <xjordward@gmail.com>
2019-03-11 04:20:36 +0300

    god jesus pls don`t fuck up

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const chatData = [
   }
 ];
 
-//app.set("port", process.env.PORT || 3001);
+app.set("port", process.env.PORT || 3001);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
