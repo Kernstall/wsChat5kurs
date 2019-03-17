b0a57a1cdba611c9cc12296f3eaf32c5dee3f696
Raman Buyak <xjordward@gmail.com>
2019-03-17 19:46:58 +0300

    for lets encrypt check

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-var srv = require('node-static');
-var file = new srv.Server();
-require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    console.log('dick');
-    file.serve(request, response);
-  }).resume();
-}).listen(process.env.PORT || 3000);
+var finalhandler = require('finalhandler')
+var http         = require('http')
+var Router       = require('router')
+
+var router = Router()
+
+// Add this for Let's Encrypt ACME challenge validation
+router.get('/.well-known/acme-challenge/:str', function (req, res) {
+  res.send(process.env.LETS_ENCRYPT_CHALLENGE);
+})
+
+var server = http.createServer(function(req, res) {
+  router(req, res, finalhandler(req, res))
+})
+
+server.listen(process.env.PORT || 3001)
